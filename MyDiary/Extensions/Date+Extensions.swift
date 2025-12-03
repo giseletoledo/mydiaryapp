55d9cc70040072132ea55c2ded24e572b0cc8072
Gisele <gisesonia@gmail.com>
2025-12-03 11:39:37 -0300

    extension de datas

--- MyDiary/Extensions/Date+Extensions.swift
+++ MyDiary/Extensions/Date+Extensions.swift
@@ -5,34 +5,21 @@
 //  Created by GISELE TOLEDO on 01/12/25.
 //
 
-import SwiftUI
 import Foundation
 
-// Extension para Date
 extension Date {
     func formatDate() -> String {
         let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        formatter.timeStyle = .short
+        
+        if Calendar.current.isDateInToday(self) {
+            formatter.dateFormat = "'Hoje' HH:mm"
+        } else if Calendar.current.isDateInYesterday(self) {
+            formatter.dateFormat = "'Ontem' HH:mm"
+        } else {
+            formatter.dateFormat = "dd/MM/yyyy HH:mm"
+        }
+        
         formatter.locale = Locale(identifier: "pt_BR")
         return formatter.string(from: self)
-    }
-}
-
-// Extension para View (animação simples)
-extension View {
-    func fadeIn() -> some View {
-        self.modifier(FadeInModifier())
-    }
-}
-
-struct FadeInModifier: ViewModifier {
-    @State private var show = false
-    
-    func body(content: Content) -> some View {
-        content
-            .opacity(show ? 1 : 0)
-            .animation(.easeIn(duration: 0.3), value: show)
-            .onAppear { show = true }
     }
 }
